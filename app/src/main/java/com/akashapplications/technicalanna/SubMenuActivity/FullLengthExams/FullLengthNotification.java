6274b8de310fa591b915becda0514046b90ac68c
Akash Giri <akash@Akashs-MacBook-Pro.local>
2019-04-15 21:03:27 +0530

    Notification and syllabus done

---
+++ app/src/main/java/com/akashapplications/technicalanna/SubMenuActivity/FullLengthExams/FullLengthNotification.java
@@ -0,0 +1,146 @@
+package com.akashapplications.technicalanna.SubMenuActivity.FullLengthExams;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.akashapplications.technicalanna.Adapters.ExamListAdapter;
+import com.akashapplications.technicalanna.HelperActivity.ExamPreview;
+import com.akashapplications.technicalanna.Models.QuizModel;
+import com.akashapplications.technicalanna.Models.SubjectExamsModel;
+import com.akashapplications.technicalanna.R;
+import com.akashapplications.technicalanna.Utils.API;
+import com.akashapplications.technicalanna.Utils.RequestQueueSingleton;
+import com.akashapplications.technicalanna.Utils.Tokens;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class FullLengthNotification extends AppCompatActivity {
+
+    ListView listView;
+    ProgressBar progressBar;
+    static String subject = "null";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_length_notification);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        subject = getIntent().getStringExtra("subject");
+        getSupportActionBar().setTitle(subject);
+
+        listView = findViewById(R.id.listview);
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.getIndeterminateDrawable().setColorFilter(
+                getResources().getColor(R.color.blue_button),
+                android.graphics.PorterDuff.Mode.SRC_IN);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new GetAllNotification().execute();
+
+    }
+
+    private class GetAllNotification extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            JSONObject reqBody = new JSONObject();
+            try {
+                reqBody.put("subject",subject);
+                reqBody.put("name","notification");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, API.FULL_EXAM_GET_DETAIL_BY_NAME, reqBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            if (progressBar.getVisibility() == View.VISIBLE)
+                                progressBar.setVisibility(View.GONE);
+                            ArrayList<String> titleList = new ArrayList<>();
+                            if(response.has("res")) {
+
+                                try {
+                                    response = response.getJSONObject("res");
+                                    JSONArray arr = response.getJSONArray("notification");
+                                    for (int i = 0; i < arr.length(); i++) {
+                                        titleList.add(arr.getString(i));
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            ArrayAdapter adapter = new ArrayAdapter<String>(FullLengthNotification.this,
+                                    android.R.layout.simple_list_item_1, titleList);
+                            listView.setAdapter(adapter);
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                                    startActivity(new Intent(getBaseContext(), ExamPreview.class).putExtra("exam",list.get(position)));
+                                }
+                            });
+
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    if (progressBar.getVisibility() == View.VISIBLE)
+                        progressBar.setVisibility(View.GONE);
+                    NetworkResponse networkResponse = error.networkResponse;
+                    Toast.makeText(getBaseContext(), "failed " + new String(networkResponse.data), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            jsonObjectRequest.setShouldCache(false);
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    0,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+            ));
+            RequestQueue requestQueue = RequestQueueSingleton.getInstance(getBaseContext())
+                    .getRequestQueue();
+            requestQueue.getCache().clear();
+            requestQueue.add(jsonObjectRequest);
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
--- app/src/main/java/com/akashapplications/technicalanna/Test/TestContent.java
+++ app/src/main/java/com/akashapplications/technicalanna/Test/TestContent.java
@@ -1,6 +1,8 @@
 package com.akashapplications.technicalanna.Test;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,14 +11,33 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.akashapplications.technicalanna.R;
 import com.akashapplications.technicalanna.SubMenuActivity.FullLengthExams.FullLengthAllExams;
+import com.akashapplications.technicalanna.SubMenuActivity.FullLengthExams.FullLengthNotification;
 import com.akashapplications.technicalanna.SubMenuActivity.SubjectWiseExam.SubjectAllExam;
+import com.akashapplications.technicalanna.Utils.API;
+import com.akashapplications.technicalanna.Utils.RequestQueueSingleton;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.yarolegovich.lovelydialog.LovelyProgressDialog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class TestContent extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ListView listView;
     String subject = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +52,7 @@ public class TestContent extends AppCompatActivity implements AdapterView.OnItem
 
         listView = findViewById(R.id.listview);
 
-        String itemArray[] = {"Notification","Syllabus","Previous Year question Paper","Full Length Tests"};
+        String itemArray[] = {"Notification", "Syllabus", "Previous Year question Paper", "Full Length Tests"};
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, itemArray);
@@ -47,8 +68,7 @@ public class TestContent extends AppCompatActivity implements AdapterView.OnItem
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home)
-        {
+        if (item.getItemId() == android.R.id.home) {
             finish();
         }
 
@@ -57,21 +77,91 @@ public class TestContent extends AppCompatActivity implements AdapterView.OnItem
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        switch (position)
-        {
+        switch (position) {
             case 0:
-
+                startActivity(new Intent(getBaseContext(), FullLengthNotification.class).putExtra("subject", subject));
                 break;
 
             case 1:
+                new FetchSyllabus().execute();
                 break;
 
             case 2:
                 break;
 
             case 3:
-                startActivity(new Intent(getBaseContext(), FullLengthAllExams.class).putExtra("subject",subject));
+                startActivity(new Intent(getBaseContext(), FullLengthAllExams.class).putExtra("subject", subject));
                 break;
+        }
+    }
+
+
+
+    private class FetchSyllabus extends AsyncTask<Void,Void,Void> {
+        LovelyProgressDialog progressDialog = new LovelyProgressDialog(TestContent.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog
+                    .setIcon(R.drawable.update)
+                    .setTitle("Please wait...")
+                    .setCancelable(false)
+                    .setMessage("Redirecting to Syllabus page")
+                    .setTopColorRes(R.color.blue_button);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            JSONObject reqBody = new JSONObject();
+            try {
+                reqBody.put("subject",subject);
+                reqBody.put("name","syllabus");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, API.FULL_EXAM_GET_DETAIL_BY_NAME, reqBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            progressDialog.dismiss();
+                            if(response.has("res")) {
+                                try {
+                                    response = response.getJSONObject("res");
+                                    Toast.makeText(getBaseContext(),"Redirecting...",Toast.LENGTH_LONG).show();
+                                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(response.getString("syllabus")));
+                                    startActivity(browserIntent);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            else
+                            {
+                                Toast.makeText(getBaseContext(),"Syllabus not added by admin",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    NetworkResponse networkResponse = error.networkResponse;
+                    Toast.makeText(getBaseContext(), "failed " + new String(networkResponse.data), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            jsonObjectRequest.setShouldCache(false);
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    0,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+            ));
+            RequestQueue requestQueue = RequestQueueSingleton.getInstance(getBaseContext())
+                    .getRequestQueue();
+            requestQueue.getCache().clear();
+            requestQueue.add(jsonObjectRequest);
+            return null;
         }
     }
 }
